f6887fad5eae15d9aa05e3540fe7ff472da17d2e
Tia Jin <happysage@0587357375.wireless.umich.net>
2018-03-07 21:30:32 -0500

    Fix indent

--- test_rpn.py
+++ test_rpn.py
@@ -16,5 +16,5 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
     def test_exponent(self):
-	result = rpn.calculate("2 2 ^")
-	self.assertEqual(4, result)
+        result = rpn.calculate("2 2 ^")
+        self.assertEqual(4, result)
